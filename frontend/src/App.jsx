709f8a5563d96f9adfce6a273e138df55caa63ba
Daniel Esteban <danielestebansa@gmail.com>
2026-04-17 15:46:27 -0500

    Fix: Import getProductos from services/api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { getProductos, getVentas, createVenta } from './services/api'
 import ProductManager from './components/ProductManager'
 import PaymentModal from './components/PaymentModal'
 import ReceiptModal from './components/ReceiptModal'
